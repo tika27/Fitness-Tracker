319ca65197b41b3c89ff60bd9a806d0285b58125
tika27 <77032551+tika27@users.noreply.github.com>
2021-03-17 18:43:55 -0600

    fixing ap

--- server.js
+++ server.js
@@ -19,8 +19,8 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
     useFindAndModify: false
 });
 
- app.use(require("./routes/htmlRoutes.js")(app));
- app.use(require("./routes/apiRoutes.js")(app));
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
